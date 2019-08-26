6a45df9a96a6403cfe923ef4a6ee08332c60b48d
mpsido <1979371837@qq.com>
2019-08-26 09:56:13 +0800

    Use and fix userMiddleware

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -14,7 +14,7 @@ import { initDBConnectionAsync } from './db_connection';
 import { Handlers } from './handlers';
 import { errorHandler } from './middleware/error_handling';
 import { urlParamsParsing } from './middleware/url_params_parsing';
-// import { userMiddleware } from './middleware/users';
+import { userMiddleware } from './middleware/users';
 import { utils } from './utils';
 
 (async () => {
@@ -35,37 +35,37 @@ import { utils } from './utils';
      * GET AssetPairs endpoint retrieves a list of available asset pairs and the information required to trade them.
      * http://sra-spec.s3-website-us-east-1.amazonaws.com/#operation/getAssetPairs
      */
-    app.get('/v2/asset_pairs', asyncHandler(Handlers.assetPairsAsync.bind(Handlers)));
+    app.get('/v2/asset_pairs', userMiddleware, asyncHandler(Handlers.assetPairsAsync.bind(Handlers)));
     /**
      * GET Orders endpoint retrieves a list of orders given query parameters.
      * http://sra-spec.s3-website-us-east-1.amazonaws.com/#operation/getOrders
      */
-    app.get('/v2/orders', asyncHandler(handlers.ordersAsync.bind(handlers)));
+    app.get('/v2/orders', userMiddleware, asyncHandler(handlers.ordersAsync.bind(handlers)));
     /**
      * GET Orderbook endpoint retrieves the orderbook for a given asset pair.
      * http://sra-spec.s3-website-us-east-1.amazonaws.com/#operation/getOrderbook
      */
-    app.get('/v2/orderbook', asyncHandler(handlers.orderbookAsync.bind(handlers)));
+    app.get('/v2/orderbook', userMiddleware, asyncHandler(handlers.orderbookAsync.bind(handlers)));
     /**
      * POST Order config endpoint retrives the values for order fields that the relayer requires.
      * http://sra-spec.s3-website-us-east-1.amazonaws.com/#operation/getOrderConfig
      */
-    app.post('/v2/order_config', Handlers.orderConfig.bind(Handlers));
+    app.post('/v2/order_config', userMiddleware, Handlers.orderConfig.bind(Handlers));
     /**
      * GET FeeRecepients endpoint retrieves a collection of all fee recipient addresses for a relayer.
      * http://sra-spec.s3-website-us-east-1.amazonaws.com/v2/fee_recipients
      */
-    app.get('/v2/fee_recipients', Handlers.feeRecipients.bind(Handlers));
+    app.get('/v2/fee_recipients', userMiddleware, Handlers.feeRecipients.bind(Handlers));
     /**
      * POST Order endpoint submits an order to the Relayer.
      * http://sra-spec.s3-website-us-east-1.amazonaws.com/#operation/postOrder
      */
-    app.post('/v2/order', asyncHandler(handlers.postOrderAsync.bind(handlers)));
+    app.post('/v2/order', userMiddleware, asyncHandler(handlers.postOrderAsync.bind(handlers)));
     /**
      * GET Order endpoint retrieves the order by order hash.
      * http://sra-spec.s3-website-us-east-1.amazonaws.com/#operation/getOrder
      */
-    app.get('/v2/order/:orderHash', asyncHandler(Handlers.getOrderByHashAsync.bind(Handlers)));
+    app.get('/v2/order/:orderHash', userMiddleware, asyncHandler(Handlers.getOrderByHashAsync.bind(Handlers)));
 
     app.post('/v2/signup', asyncHandler(signup));
     app.post('/v2/login', asyncHandler(login));
--- ts/src/middleware/users.ts
+++ ts/src/middleware/users.ts
@@ -5,17 +5,26 @@ import * as _ from 'lodash';
 import { utils } from '../utils';
 
 export function userMiddleware(req: express.Request, res: express.Response, next: express.NextFunction): void {
-    utils.log('User Middleware', req, res);
     try {
         const tokenSecret = _.isEmpty(process.env.TOKEN_SECRET)
             ? 'RANDOM_SECRET'
             : (process.env.TOKEN_SECRET as string);
-        const token = (req.headers as any).authorization.split(' ')[1];
+        let request;
+        if (req.method == 'POST') {
+            request = req.body as any;
+        } else if (req.method == 'GET') {
+            request = req.query as any;
+        } else {
+            throw new Error('User Middleware only works with POST and GET requests');
+        }
+        const token = request.authorization.split(' ')[1];
+        utils.log(`User Middleware ${req.method}!`, token, request.userId);
         const decodedToken = jwt.verify(token, tokenSecret);
-        const userId = (decodedToken as any).userId;
-        if (req.body.userId && req.body.userId !== userId) {
+        const userId = (req.query as any).userId;
+        if (req.query.userId && decodedToken != userId) {
             throw new Error('Invalid user ID');
         } else {
+            utils.log('Authentication success');
             next();
         }
     } catch {
